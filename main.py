fa4e179351e3df53e0bfe54af6388c20429dd678
nurimova <n.b.sitora@gmail.com>
2024-12-07 12:21:27 +0500

    funksiya

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 def main(S, d):
     '''create a babylonian function.
-    
     Args:
         S (int): number
         d (int): numnber
@@ -8,4 +7,9 @@ def main(S, d):
     Returns:
         float: result
     '''
-    return
+    a=(S-d**2)/2*d
+    b=a+d
+    x=b-a**2/2*b
+    return x
+s=main(S=16,  d=4)
+print(s)
